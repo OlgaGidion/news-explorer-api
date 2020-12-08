dcecf2c754ef89e93c5d76cb1a9c66eeea3adaf8
Olga Gidion <olga.gidion@gmail.com>
2020-12-08 22:55:40 +0100

    Created models

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const NotAuthorizedError = require('../errors/NotAuthorizedError');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = async function f(email, password) {
+  const user = await this.findOne({ email }).select('+password');
+  if (!user) {
+    throw new NotAuthorizedError('Неправильная почта или пароль');
+  }
+
+  const arePasswordsMatched = await bcrypt.compare(password, user.password);
+  if (!arePasswordsMatched) {
+    throw new NotAuthorizedError('Неправильная почта или пароль');
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model('user', userSchema);
